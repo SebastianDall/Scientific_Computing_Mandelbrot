5ce2df0d0d0cf4536b64ba6f0c7c27a4231ce3c5
“SebastianDall” <semoda@bio.auu.dk>
2023-06-22 11:03:30 +0000

    added new documentation

--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -2,7 +2,23 @@ import numpy as np
 from numba import jit
 from typing import Callable, Optional
 
+# No-op for use with profiling and test
+try:
 
+    @profile
+    def f(x):
+        return x
+
+except:
+
+    def profile(func):
+        def inner(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return inner
+
+
+@profile
 def enumerate_mandelbrot_set(
     C: np.ndarray,
     max_iters: int,
@@ -42,6 +58,7 @@ def enumerate_mandelbrot_set(
     return MandelbrotSet
 
 
+@profile
 def calculate_mandelbrot_naive(c: complex, max_iters: int = 100) -> float:
     """The naive implementation of the Mandelbrot set.
 
@@ -79,6 +96,7 @@ def calculate_mandelbrot_naive(c: complex, max_iters: int = 100) -> float:
     return 1
 
 
+@profile
 def calculate_mandelbrot_vectorized(C: np.ndarray, max_iters: int = 100) -> np.ndarray:
     """the vectorized implementation of the Mandelbrot set.
 
@@ -112,6 +130,53 @@ def calculate_mandelbrot_vectorized(C: np.ndarray, max_iters: int = 100) -> np.n
 
         # Check if z has escaped
         escaped_this_iter = np.abs(z) >= 2
+
+        # Update the mandelbrot set
+        mandelbrotSet = np.where(
+            escaped_this_iter & ~escaped, (i + 1) / max_iters, mandelbrotSet
+        )
+        # Update the escaped array. The | is a bitwise OR i.e. if either of the elements is true, the result is true
+        escaped = escaped | escaped_this_iter
+
+    mandelbrotSet = np.where(mandelbrotSet == 0, 1, mandelbrotSet)
+    return mandelbrotSet
+
+
+def calculate_mandelbrot_vectorized_optim(
+    C: np.ndarray, max_iters: int = 100
+) -> np.ndarray:
+    """the vectorized implementation of the Mandelbrot set.
+
+    This version is the vectorized implementation of the Mandelbrot set. It uses numpy arrays and operations to
+    calculate the Mandelbrot set for each element in C.
+
+    Args:
+        C:
+            A 2D array of complex numbers.
+        max_iters:
+            The maximum number of iterations.
+
+    Returns:
+        A 2D array of the number of iterations it took to escape.
+
+    Examples:
+        C = np.array([[-2 + 1j, -2 + 1j], [-2 + 1j, -2 + 1j]])
+        calculate_mandelbrot_vectorized(C, 100)
+
+    """
+    # Initialize z and c
+    z = np.zeros(C.shape, np.complex128)
+    mandelbrotSet = np.zeros(C.shape, np.float64)
+    escaped = np.zeros_like(mandelbrotSet, dtype=bool)
+
+    # Iterate until the maximum number of iterations is reached
+    for i in range(max_iters):
+        # Calculate the next value of z only for those elements where z has not escaped yet. The ~ is a bitwise NOT i.e it flips true to false and vice versa
+        mask = ~escaped
+        z[mask] = z[mask] ** 2 + C[mask]
+
+        # Check if z has escaped
+        escaped_this_iter = (z.real**2 + z.imag**2) >= 4
 
         # Update the mandelbrot set
         mandelbrotSet = np.where(
--- mandelbrot/multiprocessing_mandelbrot.py
+++ mandelbrot/multiprocessing_mandelbrot.py
@@ -4,21 +4,56 @@ import numpy as np
 
 
 # define the workers
-def worker(args):
+def worker(args: tuple) -> np.ndarray:
+    """Define the worker function for multiprocessing
+
+    This function is used to define the worker function for multiprocessing. It takes a tuple of arguments to be used in the
+    calculation of the Mandelbrot set and returns the result of the calculation.
+
+    Args:
+        args:
+            A tuple of arguments to be used in the calculation of the Mandelbrot set.
+            (C_slice, max_iters)
+
+    Returns:
+        A 2D array of the number of iterations it took to escape.
+
+
+    """
+
     C_slice, max_iters = args
     return calculate_mandelbrot_vectorized(C_slice, max_iters)
 
 
-def calculate_mandelbrot_multithreaded(C, max_iters, cpus):
-    """
-    This function calculates the Mandelbrot set using multiple threads
+def calculate_mandelbrot_multithreaded(
+    C: np.ndarray, max_iters: int, cpus: int
+) -> np.ndarray:
+    """Multithreaded implementation for calculating the Mandelbrot set
 
-    The Mandelbrot is calculated with the vectorized implementation
 
-    :param C: A 2D array of complex numbers
-    :param max_iters: The maximum number of iterations
-    :param cpus: The number of CPUs to use
-    :return: A 2D array of the number of iterations it took to escape
+    This function calculates the Mandelbrot set for a given array of complex numbers. It uses the multiprocessing module
+    to calculate the Mandelbrot set in parallel using the calculate_mandelbrot_vectorized function. The number of CPUs
+    declared are used to slice the array of complex numbers into equal parts and calculate the Mandelbrot set for each.
+
+    Args:
+        C:
+            A 2D array of complex numbers
+        max_iters:
+            The maximum number of iterations
+        cpus:
+            The number of CPUs to use
+
+    Returns:
+        A 2D array of the number of iterations it took to escape
+
+
+    Raises:
+        ValueError:
+            If cpus is not in the range 1 to the number of CPUs available on the system
+
+    Examples:
+        calculate_mandelbrot_multithreaded(np.array([[-2, 2], [-2, 2]]), 100, 2)
+
     """
 
     # create the pool
@@ -27,6 +62,7 @@ def calculate_mandelbrot_multithreaded(C, max_iters, cpus):
 
     pool = multiprocessing.Pool(cpus)
 
+    # slice the array of complex numbers into equal parts
     slice_indices = [
         (i * C.shape[0] // cpus, (i + 1) * C.shape[0] // cpus) for i in range(cpus)
     ]
