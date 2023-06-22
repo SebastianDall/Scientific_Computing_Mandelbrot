eaebf0ff9f44df9e1124df066f4a9584fa3f88ee
“SebastianDall” <semoda@bio.auu.dk>
2023-06-22 09:05:46 +0000

    changed docstrings and added typing

--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -1,14 +1,32 @@
 import numpy as np
 from numba import jit
+from typing import Callable, Optional
 
 
-def enumerate_mandelbrot_set(C, max_iters, func=None):
-    """
-    This function calculates mandebrot for each element in C
-    :param C: A 2D array of complex numbers
-    :param max_iters: The maximum number of iterations
-    :param func: The function to use to calculate the Mandelbrot set
-    :return: A 2D array of the number of iterations it took to escape
+def enumerate_mandelbrot_set(
+    C: np.ndarray,
+    max_iters: int,
+    func: Optional[Callable[[np.ndarray, int], np.ndarray]] = None,
+) -> np.ndarray:
+    """This function calculates mandebrot for each element in C.
+
+    This function loops through every element in C and calculates the number of iterations it takes to escape.
+
+    Args:
+        C:
+            A 2D array of complex numbers.
+        max_iters:
+            The maximum number of iterations.
+        func:
+            A naive function to use to calculate the Mandelbrot set.
+
+    Returns:
+        A 2D array of the number of iterations it took to escape.cape
+
+    Examples:
+        C = np.array([[-2 + 1j, -2 + 1j], [-2 + 1j, -2 + 1j]])
+        enumerate_mandelbrot_set(C, 100, calculate_mandelbrot_naive)
+
     """
 
     if func is None:
@@ -24,13 +42,26 @@ def enumerate_mandelbrot_set(C, max_iters, func=None):
     return MandelbrotSet
 
 
-# This function is the naive implementation of the Mandelbrot set
-def calculate_mandelbrot_naive(c, max_iters):
-    """
-    This function is the naive implementation of the Mandelbrot set
-    :param c: The complex number to check
-    :param max_iters: The maximum number of iterations
-    :return: The number of iterations it took to escape
+def calculate_mandelbrot_naive(c: complex, max_iters: int = 100) -> float:
+    """The naive implementation of the Mandelbrot set.
+
+    This function is the naive implementation of the Mandelbrot set. The naive approach is to disregard any vectorization and
+    instead take a single value, c, and iterate until it escapes or the maximum number of iterations is reached.
+    It is to be used with enumerate_mandelbrot_set
+
+
+    Args:
+        c:
+            A complex number.
+        max_iters:
+            The maximum number of iterations.
+
+    Returns:
+        The number of iterations it took to escape.
+
+    Examples:
+        calculate_mandelbrot_naive(-2 + 1j, 100)
+
     """
     # Initialize z and c
     z = 0
@@ -48,15 +79,25 @@ def calculate_mandelbrot_naive(c, max_iters):
     return 1
 
 
-# This function is the vectorized implementation of the Mandelbrot set
-def calculate_mandelbrot_vectorized(C, max_iters):
-    """
+def calculate_mandelbrot_vectorized(C: np.ndarray, max_iters: int = 100) -> np.ndarray:
+    """the vectorized implementation of the Mandelbrot set.
+
     This version is the vectorized implementation of the Mandelbrot set. It uses numpy arrays and operations to
     calculate the Mandelbrot set for each element in C.
 
-    :param C: A 2D array of complex numbers
-    :param max_iters: The maximum number of iterations
-    :return: A 2D array of the number of iterations it took to escape
+    Args:
+        C:
+            A 2D array of complex numbers.
+        max_iters:
+            The maximum number of iterations.
+
+    Returns:
+        A 2D array of the number of iterations it took to escape.
+
+    Examples:
+        C = np.array([[-2 + 1j, -2 + 1j], [-2 + 1j, -2 + 1j]])
+        calculate_mandelbrot_vectorized(C, 100)
+
     """
     # Initialize z and c
     z = np.zeros(C.shape, np.complex128)
@@ -65,7 +106,7 @@ def calculate_mandelbrot_vectorized(C, max_iters):
 
     # Iterate until the maximum number of iterations is reached
     for i in range(max_iters):
-        # Calculate the next value of z only for those elements where z has not escaped yet
+        # Calculate the next value of z only for those elements where z has not escaped yet. The ~ is a bitwise NOT i.e it flips true to false and vice versa
         mask = ~escaped
         z[mask] = z[mask] ** 2 + C[mask]
 
@@ -76,7 +117,7 @@ def calculate_mandelbrot_vectorized(C, max_iters):
         mandelbrotSet = np.where(
             escaped_this_iter & ~escaped, (i + 1) / max_iters, mandelbrotSet
         )
-        # Update the escaped array
+        # Update the escaped array. The | is a bitwise OR i.e. if either of the elements is true, the result is true
         escaped = escaped | escaped_this_iter
 
     mandelbrotSet = np.where(mandelbrotSet == 0, 1, mandelbrotSet)
@@ -85,12 +126,24 @@ def calculate_mandelbrot_vectorized(C, max_iters):
 
 # This function is jit version of the naive implementation of the Mandelbrot set
 @jit(nopython=True)
-def calculate_mandelbrot_naive_with_numba(c, max_iters):
-    """
-    This function is the naive implementation of the Mandelbrot set
-    :param c: The complex number to check
-    :param max_iters: The maximum number of iterations
-    :return: The number of iterations it took to escape
+def calculate_mandelbrot_naive_with_numba(c: complex, max_iters: int = 100) -> float:
+    """jit version of the naive implementation of the Mandelbrot set.
+
+    This function is the naive implementation of the Mandelbrot set with numba. The naive approach is to disregard any vectorization and
+    instead take a single value, c, and iterate until it escapes or the maximum number of iterations is reached. This function is the same as calculate_mandelbrot_naive
+    It is to be used with enumerate_mandelbrot_set.
+
+    Args:
+        c:
+            A complex number.
+        max_iters:
+            The maximum number of iterations.
+
+    Returns:
+        The number of iterations it took to escape.
+
+    Examples:
+        calculate_mandelbrot_naive_with_numba(-2 + 1j, 100)
     """
     # Initialize z and c
     z = 0
